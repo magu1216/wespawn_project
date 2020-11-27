4f03664c409e2c105c66a32f529f1503f61df4ba
magu1216 <61665279+magu1216@users.noreply.github.com>
2020-11-27 15:45:19 +0100

    Update and rename Header.js to AppHeader.js

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,24 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigation from "./Navigation";
 
 export default function Header() {
   return (
-    <StyledHeader>
+    <Header>
       <h2>weSPAWN</h2>
       <h1>Profile</h1>
-      <Router>
-        <Navigation />
-        <Switch>
-          <Route path="/" />
-        </Switch>
-      </Router>
-    </StyledHeader>
+      <Navigation />
+    </Header>
   );
 }
 
-const StyledHeader = styled.header`
+const Header = styled.header`
   background-color: #e5e5e5;
   display: flex;
   justify-content: space-between;
