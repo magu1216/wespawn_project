1ae95c2ee3e108f41a06689a79e3e41c6c6f81c9
Markus Guertner <guertnermarkus@gmail.com>
2020-12-23 11:17:31 +0100

    minor heroku changes

--- client/src/services/getPlatformList.js
+++ client/src/services/getPlatformList.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 
 async function getPlatformList() {
-  const response = await axios.get(
-    "https://frozen-gorge-61178.herokuapp.com/platforms"
-  );
+  const response = await axios.get("https://wespawn.herokuapp.com/platforms");
   const platforms = response.data;
   return platforms;
 }
 
 export async function getPictures() {
-  const response = await axios.get("http://localhost:4000/picture");
+  const response = await axios.get("https://wespawn.herokuapp.com/picture");
   return response.data;
 }
 
